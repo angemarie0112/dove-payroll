958c0d149f356242ff0ec6b1c97456cc348c5e97
Bockarie D. Marah <bockarie.marah@gmail.com>
2021-05-15 17:21:18 +0000

    Complete the method to add a tax region to the datasource

--- src/com/dovePayroll/models/Model_tax_region.java
+++ src/com/dovePayroll/models/Model_tax_region.java
@@ -1,12 +1,32 @@
 package com.dovePayroll.models;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.dovePayroll.doveTypes.TaxRegion;
 
 public class Model_tax_region extends DBConnect {
-	private int[] addTaxRegion(TaxRegion region) {
-		return null; // return the regionId and status
+	private int addTaxRegion(TaxRegion region) throws SQLException {
+		// create the query string
+		String sql = "INSERT INTO `taxRegion`(`payeId`, `pensionRate`, `medicalInsuranceRate`) VALUES (?,?,?)";
+
+		// create a prepared statement for the insertion
+		PreparedStatement ps = this.con().prepareStatement(sql);
+
+		// set the values for the prepared statement
+		ps.setInt(1, region.getPaye().getPayeID());
+		ps.setDouble(2, region.getPensionRate());
+		ps.setDouble(3, region.getMedicalInsurance());
+
+		// execute the prepared statement 
+		int rs = ps.executeUpdate();
+
+		// close the connection and the prepared statement
+		this.con().close();
+		ps.close();
+
+		return rs; // return the regionId and status
 	}
 	
 	private int updateTaxRegion(TaxRegion region) {
